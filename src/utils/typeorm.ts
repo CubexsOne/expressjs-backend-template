4824f736d0070012d7aa0542af986294b5053d15
Pascal Sauer <email@cubexs.dev>
2022-11-02 20:15:52 +0100

    Add ssl options for k8s

--- src/utils/typeorm.ts
+++ src/utils/typeorm.ts
@@ -1,5 +1,15 @@
-import { DataSource, LoggerOptions } from 'typeorm'
+import { DataSource } from 'typeorm'
 import { environment } from './environment'
+
+interface SSLOptions {
+  rejectUnauthorized?: boolean
+}
+let ssl: SSLOptions | undefined = undefined
+if (environment.NODE_ENV === 'production') {
+  ssl = {
+    rejectUnauthorized: false,
+  }
+}
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
@@ -13,4 +23,5 @@ export const AppDataSource = new DataSource({
   entities: [__dirname + '/../models/*{.js,.ts}'],
   subscribers: [],
   migrations: [__dirname + '/../migrations/*{.js,.ts}'],
+  ssl,
 })
