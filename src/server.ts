73914d8019aae451d4d5635eb42642306ea079c2
Pascal Sauer <email@cubexs.dev>
2022-11-02 18:16:36 +0100

    Initialize DB

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,13 @@ import 'reflect-metadata'
 import express from 'express'
 import controller from './controller'
 import logger from './utils/logger'
+import { AppDataSource } from './utils/typeorm'
 
 const App = express()
 App.use(express.json())
 
 App.use('/api', controller)
 App.listen(3000, () => {
+  AppDataSource.initialize()
   logger.info('Server is running...')
 })
